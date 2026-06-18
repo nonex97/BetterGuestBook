d37f02c98279ace8c3cf4b984f160dd2bc868cc9
nonex97 <ngrguric97@gmail.com>
2026-06-18 21:13:30 +0200

    Added requirements

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Capture the information about each guest
+// Info to capture: First name, last name, message to the host
+// Once done, loop through each guest and print their info
+
 namespace ConsoleUI
 {
     internal class Program
